da0b27802cb78a1026f71254ac1ad00b6e584d8b
Nick.Na <naguangqing@qq.com>
2019-06-26 23:25:27 +0800

    (h5) 项目初始化; (backend) 允许跨域

---
+++ h5/src/views/Page1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page1">
+    page 1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page1',
+  created () {
+    this.show()
+  },
+  methods: {
+    show () {
+      this.$http({
+        method: 'get',
+        url: '/api/hello'
+      }).then(function (response) {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
--- web/web/settings.py
+++ web/web/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
